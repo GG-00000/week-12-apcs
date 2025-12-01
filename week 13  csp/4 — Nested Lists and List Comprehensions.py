a3678b5ae6720ce038a184f99ad8b05811c9381b
GG-00000 <ggutierrez48@cps.edu>
2025-12-01 20:35:37 +0000

    Lits

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -1,4 +1,46 @@
 # Objective:
+
+list1 = [1, 2, 3]
+lits2 = [4, 5, 6]
+nested_list = [list1, lits2]
+
+print(nested_list)
+print(nested_list[1][2])
+fruits = ["apple", "orange","banana","coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats = ["chicken", "fish","turkey"]
+
+groceries = [fruits, vegetables, meats]
+fruits[0]
+print(groceries[2][2])
+for collection in groceries:
+    for food in collection:
+        print(food, end= "")
+    print()
+
+num_pad = ((1, 2, 3),
+           (4, 5, 6)
+           (7, 8, 9)
+           ("*", 0,"#"))
+
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+        print()
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(matrix[1][2])
+print(matrix[0][1])
+print(matrix[0][0])
+
+
+
+            
+
 # Students will manipulate nested lists and understand basic list comprehensions.
 
 # Key Notes:
@@ -18,13 +60,10 @@
 
 # Examples:
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+print(matrix[1][2])
+print(matrix[0][1])
+print(matrix[0][0])
 
-print(matrix[1][2])    # 6
 
 # List comprehension
 first_col = [row[0] for row in matrix]
@@ -43,3 +82,6 @@ print(first_col)       # [1, 4, 7]
 # Use a list comprehension to extract the last item from each sub-list.
 
 # Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
+squared_numbers = [x** for x in range(1, 11)]
+
+print(squared_numbers)
